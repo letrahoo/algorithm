001d00197d6889cab53b3f89908d5ed7a3c6e320
letra <letra@bingyan.net>
2018-10-18 20:05:53 +0800

    :construction: feat: init BFamily file structure
    add initial BFamily Files

---
+++ basic/Graph/BFS.js
@@ -0,0 +1,9 @@
+const colors = {
+  WHITE: 'white', //白色
+  GRAY: 'gray', //灰色
+  BLACK: 'black', //黑色
+};
+
+function BFS(graph, s) {
+  
+}
---
+++ basic/Tree/BFamily/BPlusTree.js
@@ -0,0 +1,5 @@
+class BPlusTree {
+  constructor() {
+    
+  }
+}
